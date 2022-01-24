eaa09a783d06939f26b581f054b68ce59b0d227d
Bridget Falkenhayn <bfalkenhayn01@gmail.com>
2022-01-24 13:54:38 -0500

    Added two buttons and messages

--- Challenge 1/ViewController.swift
+++ Challenge 1/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        }
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBAction func oneButton(_ sender: Any) {messageLabel.text="You're doing great"
     }
-
-
+    @IBAction func twoButton(_ sender: Any) {messageLabel.text="Reevaluate your decision"
+    }
 }
 
